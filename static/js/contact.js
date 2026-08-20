6b78f8e83c63655e3b6abf4c58c1bbaf50c43bc5
Aldane Hutchinson <adminalchicken@gmail.com>
2026-08-20 04:56:12 -0500

    hh

---
+++ static/js/contact.js
@@ -0,0 +1,9 @@
+    document.querySelectorAll('.alert-success').forEach(function(alert) {
+        setTimeout(function() {
+            alert.style.transition = 'opacity 0.5s';
+            alert.style.opacity = '0';
+            setTimeout(function() {
+                alert.remove();
+            }, 500);
+        }, 5000);
+    });
